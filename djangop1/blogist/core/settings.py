00592fb232912342493b4bdaed5221ba4076186f
vivek Bajpai <112931187+mrvivek1999@users.noreply.github.com>
2022-11-29 12:42:01 +0530

    Update settings.py

--- djangop1/blogist/core/settings.py
+++ djangop1/blogist/core/settings.py
@@ -125,6 +125,11 @@ MEDIA_URL = 'media/'
 
 STATIC_ROOT = BASE_DIR.joinpath('static')
 MEDIA_ROOT = BASE_DIR.joinpath('media')
+EMAIL_HOST ='smtp.gmail.com'
+EMAIL_PORT = 587
+ENAIL_HOST_USER='bajpaivivek833@gmail.com'
+EMAIL_HOST_PASSWORD='qyefuueijkkhnqqp'
+EMAIL_USE_TLS=True
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
